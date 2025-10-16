51537d35bcdf512fc52d26573da34b965e761cef
WahajAhmedIqbal <wahaj465@gmail.com>
2025-10-17 02:46:51 +0500

    first commit-4

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000'
 
+
 export const api = axios.create({
   baseURL: API_BASE,
   headers: { 'Content-Type': 'application/json' }
